8003ff9232f05123f74564a62443ebf4e68f33ed
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-07-09 13:06:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+//variables
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
+
+//Function for setting a random square for the mole and getting the square id on which the mole is placed
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+
+    hitPosition = randomSquare.id
+}
+
+//Adding Event Listener to check if the specific square has been clicked on
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+//Moving mole to random square
+function moveMole() {
+    timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+//Finding and showing the time left in the game
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('GAME OVER! Your final score is ' + result)
+    }
+
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
